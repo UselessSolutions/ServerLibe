27829f5520e10935467bf916145d30b10144de42
WatermelonMojito <WatermelonMojitoLive@gmail.com>
2024-10-26 01:52:51 -0500

    +1

--- src/main/java/org/useless/test/tasdaas.java
+++ src/main/java/org/useless/test/tasdaas.java
@@ -12,7 +12,7 @@ import org.useless.serverlibe.config.YamlConfiguration;
 import java.io.File;
 import java.io.IOException;
 
-public class TestPlugin implements ServerLibeEntrypoint {
+public class tasdaas implements ServerLibeEntrypoint {
 	@Override
 	public void serverlibeInit() throws IOException {
 		ServerLibe.registerListener(new TestFeatureListener());
